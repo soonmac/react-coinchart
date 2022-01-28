97ef610d357a67d657370c9e4453bae41ce22f11
[Github <55690712+soonmac@users.noreply.github.com>
2022-01-29 05:20:00 +0900

    메인페이지, 코인 상세 페이지 대략 틀 잡음

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
-import { darkTheme, lightTheme } from './theme';
+import React from "react";
+import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
---
+++ src/routes/Coins.tsx
@@ -0,0 +1,108 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.accentColor};
+  font-size: 3rem;
+  font-weight: bold;
+  padding: 20px 0;
+`;
+
+const Container = styled.div`
+  max-width: 480px;
+  margin: 0 auto;
+  padding: 0px 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Loader = styled.span`
+  padding-top: 30px;
+  display: block;
+  text-align: center;
+`;
+
+const CoinsList = styled.ul``;
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  margin-bottom: 10px;
+  padding: 20px;
+  border-radius: 20px;
+  a {
+    display: flex;
+    align-items: center;
+    transition: color 0.2s ease-in;
+  }
+  &:hover {
+    background-color: ${(props) => props.theme.accentColor};
+    a {
+      color: ${(props) => props.theme.textColor};
+    }
+  }
+`;
+
+const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  margin-right: 16px;
+`;
+
+interface CoinInterface {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+}
+
+function Coins() {
+  const [coins, setCoins] = useState<CoinInterface[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    (async () => {
+      const response = await fetch("https://api.coinpaprika.com/v1/coins");
+      const json = await response.json();
+      setCoins(json.slice(0, 100));
+      setLoading(false);
+    })();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Title>COINS</Title>
+      </Header>
+      {loading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <CoinsList>
+          {coins.map((coin) => (
+            <Coin key={coin.id}>
+              <Link
+                to={{
+                  pathname: `/${coin.id}`,
+                  state: {name: coin.name},
+                }}
+              >
+                <Img
+                  src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
+                />
+                {coin.name} 👉
+              </Link>
+            </Coin>
+          ))}
+          ;
+        </CoinsList>
+      )}
+    </Container>
+  );
+}
+
+export default Coins;
